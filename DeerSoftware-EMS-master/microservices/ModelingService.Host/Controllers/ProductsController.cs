e9dc2be42e7939759b8ba468094e6e2083e051d8
chen <wancm520@163.com>
2021-06-19 10:27:12 +0800

    项目创建提交

---
+++ DeerSoftware-EMS-master/microservices/ModelingService.Host/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace ModelingService.Host
+{
+    [RemoteService]
+    [Area("productManagement")]
+    [Route("api/productManagement")]
+    public class ProductsController : AbpController
+    {
+        [HttpGet]
+        [Route("GetInfo")]
+        public Task<string> GetInfoAsync()
+        {
+            string strip = (Request.HttpContext.Connection.LocalIpAddress.MapToIPv4().ToString() + ":" + Request.HttpContext.Connection.LocalPort);
+
+            return Task.FromResult(strip);
+        }
+
+        
+    }
+}
