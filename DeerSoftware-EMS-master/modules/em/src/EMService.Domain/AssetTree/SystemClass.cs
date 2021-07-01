765ff59cf03ca33b585db2b9c44338eb0e539593
于少迪 <1004938863@qq.com>
2021-07-01 08:52:58 +0800

    设备系统实体

---
+++ DeerSoftware-EMS-master/modules/em/src/EMService.Domain/AssetTree/SystemClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace EMService.AssetTree
+{
+    [DisplayName("系统类型分组")]
+    public class SystemClass: FullAuditedAggregateRoot<Guid>
+    {
+        public SystemClass(Guid Key):base(Key)
+        {
+
+        }
+        [DisplayName("父级")]
+        public Guid ParentId { get; set; }
+        [DisplayName("名称")]
+        public string Name { get; set; }
+        [DisplayName("编码")]
+        public string Code { get; set; }
+    }
+}
--- DeerSoftware-EMS-master/modules/em/src/EMService.EntityFrameworkCore/EntityFrameworkCore/EMServiceDbContextModelCreatingExtensions.cs
+++ DeerSoftware-EMS-master/modules/em/src/EMService.EntityFrameworkCore/EntityFrameworkCore/EMServiceDbContextModelCreatingExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using EMService.AssetTree;
 using Microsoft.EntityFrameworkCore;
 using Volo.Abp;
+using Volo.Abp.EntityFrameworkCore.Modeling;
 
 namespace EMService.EntityFrameworkCore
 {
@@ -23,6 +24,28 @@ namespace EMService.EntityFrameworkCore
             {
                 b.ToTable("Tree_Base");
             });
+
+            builder.Entity<DevSystem>(b =>
+            {
+                //Configure table & schema name
+                b.ToTable(EMServiceConsts.DbTablePrefix+"Tree_System",EMServiceConsts.DbSchema);
+                b.ConfigureByConvention();
+
+                //Properties
+                b.Property(q => q.SystemGroup).IsRequired().HasMaxLength(50);
+                b.Property(q => q.SystemClass).IsRequired().HasMaxLength(50);
+            });
+
+            builder.Entity<SystemClass>(
+                b =>
+                {
+                    b.ToTable(EMServiceConsts.DbTablePrefix+"Tree_System_Class",EMServiceConsts.DbSchema);
+                    b.ConfigureByConvention();
+                    b.Property(q=>q.ParentId).HasMaxLength(50);
+                    b.Property(q=>q.Name).IsRequired().HasMaxLength(50);
+                    b.Property(q => q.Code).IsRequired().HasMaxLength(50);
+                });
+
         }
     }
 }
