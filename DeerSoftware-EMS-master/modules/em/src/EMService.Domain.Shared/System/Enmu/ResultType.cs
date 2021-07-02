8cc4436b1330f44fbf65605e9f84bc7772db6419
chen <wancm520@163.com>
2021-07-02 10:42:33 +0800

    公共返回对象

---
+++ DeerSoftware-EMS-master/modules/em/src/EMService.Domain.Shared/System/Enmu/ResultType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EMService
+{
+    /// <summary>
+    /// 返回对象类型
+    /// </summary>
+    public enum ResultType
+    {
+        /// <summary>
+        /// 成功
+        /// </summary>
+        Succeed = 1,
+        /// <summary>
+        /// 失败
+        /// </summary>
+        Error = 2
+    }
+}
