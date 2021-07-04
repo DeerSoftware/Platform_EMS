c03012374bf933d6ce3e549962accca22ec7263d
贞捷 唐 <10135185@qq.com>
2021-07-04 14:49:32 +0800

    去除BaseDto

--- DeerSoftware-EMS-master/modules/em/src/EMService.Application.Contracts/AssetTree/Dto/FoundationDto.cs
+++ DeerSoftware-EMS-master/modules/em/src/EMService.Application.Contracts/AssetTree/Dto/FoundationDto.cs
@@ -7,7 +7,7 @@ namespace EMService.AssetTree.Dto
     /// <summary>
     /// 设备树基础数据
     /// </summary>
-    public class FoundationDto:BaseDto
+    public class FoundationDto
     {
         /// <summary>
         /// 主键Id
